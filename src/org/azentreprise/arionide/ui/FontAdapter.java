7ee0d3269f20baf5bbff82d453a151a1dfa0683c
Arion <Arion@DESKTOP-6GBQRHR.home>
2017-06-16 23:25:12 +0200

    New font adapter

--- src/org/azentreprise/arionide/ui/FontAdapter.java
+++ src/org/azentreprise/arionide/ui/FontAdapter.java
@@ -20,56 +20,111 @@
  *******************************************************************************/
 package org.azentreprise.arionide.ui;
 
+import java.awt.Canvas;
 import java.awt.Font;
 import java.awt.FontMetrics;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FontAdapter {
 	
-	private static final int CACHE_STEP = 2;
-	private static final int WIDTH_CORRECTION = 4;
+	// please consider using powers of two for these variables
+	private static final int MAX_FONT_SIZE = 64;
+	private static final int STEP = 2;
 	
-	private final Font[] cache = new Font[32];
-	private String hash = new String();
+	// faster than (int) (Math.log(MAX_FONT_SIZE / STEP) / Math.log(2));
+	private static final int COMPLEXITY = 31 - Integer.numberOfLeadingZeros(MAX_FONT_SIZE / STEP);
+
+	// approximation of the width ratio
+	private static final String CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 	
-	private FontMetrics metrics = null;
-	private Font font = null;
-	private float fontSize = 0.0f;
-	private int fontHeight = 0;
+	/* 
+	 * The cache info holds information about width per char and height per char.
+	 * By the way I know that height per char isn't relevant but it is for the symmetry of the code. (make it beautiful)
+	 * The point is that these two ratios are strictly monotonically increasing.
+	 * The distribution should be approximately linear.
+	 */
+	private final List<CacheInfo> cache = new ArrayList<>();
 	
-	public FontAdapter(FontMetrics fm) {
-		this.metrics = fm;
-		this.font = this.metrics.getFont();
-		this.fontHeight = this.metrics.getHeight() + this.metrics.getAscent();
-		this.fontSize = this.font.getSize2D();
-		
-		for(int i = 0; i < this.cache.length; i++) {
-			this.cache[i] = this.font.deriveFont(i * CACHE_STEP + 1.0f);
+	private FontMetrics lastMetrics;
+	
+	public FontAdapter(Font font) {
+		Canvas fakeCanvas = new Canvas(); 
+
+		for(int i = 1; i < MAX_FONT_SIZE; i += STEP) {
+			font = font.deriveFont((float) i);
+			
+			FontMetrics metrics = fakeCanvas.getFontMetrics(font);
+			
+			float width = metrics.stringWidth(CHARS);
+			float height = metrics.getLeading() + metrics.getAscent() + metrics.getDescent();
+					
+			this.cache.add(new CacheInfo(font, metrics, width / CHARS.length(), height / CHARS.length()));
 		}
 	}
 	
+	// linear interpolation
 	public Font adapt(String str, int width, int height, float relativeSize) {
-		String localHash = "hash@" + width + height + relativeSize;
+		float iw = width * relativeSize / str.length();
+		float ih = height * relativeSize / str.length();
 		
-		if(localHash != this.hash) {
-			float fontSizeX = (float) width * relativeSize / this.metrics.stringWidth(str) / WIDTH_CORRECTION;
-			float fontSizeY = (float) height * relativeSize / this.fontHeight;
-			int size = (int) (this.fontSize * Math.min(fontSizeX, fontSizeY) / CACHE_STEP);
-			
-			if(size < 0) {
-				size = 0;
-			} else if(size >= this.cache.length) {
-				size = this.cache.length - 1;
-			}
-			
-			this.hash = localHash;
-			
-			return this.cache[size];
-		} else {
-			return null;
-		}
+		CacheInfo info = this.makeInterpolation(iw, ih);
+		
+		this.lastMetrics = info.getFontMetrics();
+		
+		return info.getFont();
 	}
 	
-	public FontMetrics getMetrics() {
-		return this.metrics;
+	// dichotomy algorithm
+	private CacheInfo makeInterpolation(float iw, float ih) {
+		
+		int current = MAX_FONT_SIZE / STEP / 2;
+		CacheInfo sample = null;
+		
+		for(int i = 0; i < COMPLEXITY; i++) {
+			sample = this.cache.get(current);
+			
+			if(sample.getWidthRatio() >= iw || sample.getHeightRatio() >= ih) {
+				current -= Integer.highestOneBit(i);
+			} else {
+				current += Integer.highestOneBit(i);
+			}
+		}
+		
+		return sample;
+	}
+	
+	public FontMetrics getLastMetrics() {
+		return this.lastMetrics;
+	}
+	
+	private static final class CacheInfo {
+		private final Font font;
+		private final FontMetrics metrics;
+		private final float widthRatio;
+		private final float heightRatio;
+		
+		private CacheInfo(Font font, FontMetrics metrics, float widthRatio, float heightRatio) {
+			this.font = font;
+			this.metrics = metrics;
+			this.widthRatio = widthRatio;
+			this.heightRatio = heightRatio;
+		}
+		
+		private Font getFont() {
+			return this.font;
+		}
+		
+		private FontMetrics getFontMetrics() {
+			return this.metrics;
+		}
+		
+		private float getWidthRatio() {
+			return this.widthRatio;
+		}
+		
+		private float getHeightRatio() {
+			return this.heightRatio;
+		}
 	}
 }
